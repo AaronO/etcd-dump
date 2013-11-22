cbc68e4a033a4e1d44516825cd1e9ec61001e65a
Aaron O'Mullan <aaron.omullan@friendco.de>
2013-11-22 10:07:13 -0800

    First version 0.0.1

---
+++ bin/etcd-dump.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+
+// Requires
+var _ = require('underscore');
+
+var fs = require('fs');
+
+// Comannder
+var prog = require('commander');
+
+// etcd-dump's package.json file
+var pkg = require('../package.json');
+
+// Dumper class
+var dumper = require('../')();
+
+// General options
+prog
+.version(pkg.version)
+.option('-f, --file [json_file]', 'Path to JSON dump file for dumping/storing', './etcd_dump.json');
+
+// Dump command
+prog
+.command('dump')
+.action(function() {
+    return dumper.dump()
+    .then(function(data) {
+        // Write file to disk
+        fs.writeFileSync(prog.file, JSON.stringify(data));
+    })
+    .done();
+});
+
+prog
+.command('restore')
+.action(function() {
+    var entries = JSON.parse(fs.readFileSync(prog.file));
+
+    return dump.restore(entries)
+    .then(function() {
+        console.log('Restore succeeded');
+    })
+    .done();
+});
+
+// Parse and fallback to help if no args
+if(_.isEmpty(prog.parse(process.argv).args)) prog.help();
---
+++ index.js
@@ -0,0 +1,54 @@
+// Requires
+var Q = require('q');
+var _ = require('underscore');
+
+var qClass = require('qpatch').qClass;
+
+// Etcd client
+var Etcd = qClass(require('node-etcd'), ['watcher']);
+
+
+// Since etcd create the dir keys automatically
+// transform the tree of keys
+// to contain only a flat array of leaves
+function cleanDump(obj) {
+    // Is a leaf
+    if(!_.has(obj, 'kvs')) {
+        // We don't want the modifiedIndex attr in our dumps/restores
+        return _.pick(obj, 'key', 'value');
+    }
+    return _.flatten(_.map(obj.kvs, cleanInput));
+}
+
+
+function Dumper(etcd) {
+    // ETCD client
+    this.store = new Etcd();
+
+    _.bindAll(this);
+}
+
+// Get a JS object of the DB
+Dumper.prototype.dump = function() {
+    return this.store.get('', {
+        recusrive: true
+    })
+    .then(cleanDump);
+};
+
+// Restore a list of keys
+Dumper.prototype.restore = function(entries) {
+    var self = this;
+
+    return Q.all(_.map(entries, function(entry) {
+        return this.store.set(entry.key, entry.value);
+    }));
+};
+
+// Restore the database from input data
+function createDumper() {
+    return new Dumper();
+}
+
+// Exports
+module.exports = createDumper;
