6cce19080827d736cd82050af60e76c74fdaa0b9
Anthony Putignano <contact@anthonyputignano.com>
2014-05-19 13:41:17 -0400

    Runtime config support

--- bin/etcd-dump.js
+++ bin/etcd-dump.js
@@ -11,13 +11,31 @@ var prog = require('commander');
 // etcd-dump's package.json file
 var pkg = require('../package.json');
 
-// Dumper class
-var dumper = require('../')();
-
 // General options
 prog
 .version(pkg.version)
-.option('-f, --file [json_file]', 'Path to JSON dump file for dumping/storing', './etcd_dump.json');
+.option('-f, --file [json_file]', 'Path to JSON dump file for dumping/storing', './etcd_dump.json')
+.option('-h, --host [127.0.0.1]', 'etcd host', '127.0.0.1')
+.option('-p, --port [4001]', 'etcd port', '4001')
+.option('--ca-file [/path/to/ca-file]', 'Path to CA file for SSL support', '')
+.option('--cert-file [/path/to/cert-file]', 'Path to cert file for SSL support', '')
+.option('--key-file [/path/to/key-file]', 'Path to key file for SSL support', '')
+.option('--reject-unauthorized [true]', 'Reject unauthorized SSL certificates?', 'true')
+.parse(process.argv);
+
+var sslOptions = {};
+['caFile', 'certFile', 'keyFile'].forEach(function(key){
+	if (prog[key]) {
+		sslOptions[key.replace("File", "")] = fs.readFileSync(prog[key]);
+	}
+});
+
+if (Object.keys(sslOptions).length > 0) {
+	sslOptions.rejectUnauthorized = prog.rejectUnauthorized === 'false' ? false : true;
+}
+
+// Dumper class
+var dumper = require('../')(prog.host, prog.port, sslOptions);
 
 // Dump command
 prog
