cc2d63636879f841e3a94500151228ab3ac76641
Tom Howe <tom.howe@mailonline.co.uk>
2014-09-08 17:37:10 +0100

    tom: add support for ip/port

--- bin/etcd-dump.js
+++ bin/etcd-dump.js
@@ -11,18 +11,21 @@ var prog = require('commander');
 // etcd-dump's package.json file
 var pkg = require('../package.json');
 
-// Dumper class
-var dumper = require('../')();
 
 // General options
 prog
 .version(pkg.version)
-.option('-f, --file [json_file]', 'Path to JSON dump file for dumping/storing', './etcd_dump.json');
+.option('-f, --file [json_file]', 'Path to JSON dump file for dumping/storing', './etcd_dump.json')
+.option('-h, --host [host]', 'Hostname to connect to', 'localhost')
+.option('-p, --port [port]', 'Port to connect to', '4001')
+;
 
 // Dump command
 prog
 .command('dump')
 .action(function() {
+    var dumper = require('../')(prog.host, prog.port);
+
     return dumper.dump()
     .then(function(data) {
         // Write file to disk
@@ -34,6 +37,7 @@ prog
 prog
 .command('restore')
 .action(function() {
+    var dumper = require('../')(prog.host, prog.port);
     var entries = JSON.parse(fs.readFileSync(prog.file));
 
     return dumper.restore(entries)
